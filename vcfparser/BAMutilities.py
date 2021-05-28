af8994f0bbe3c95b14a147cea3dae3f858773c77
Kirill Bessonov <kbessonov@gmail.com>
2021-05-28 17:16:44 -0400

    New SNV coverages per file feature implemented by writting '.cache_snv_coverages.json' for queried SNV positions ffor speedup

--- vcfparser/BAMutilities.py
+++ vcfparser/BAMutilities.py
@@ -3,12 +3,12 @@ from collections import Counter
 import numpy as np
 
 
-def get_ref_coverage_array(bamfilepath, query_positions):
+def get_ref_coverage_array(bamfilepath, query_positions, cache_dict):
     '''
     Get read counts for a given position or positions if range is > 1
     :param samfile: PySAM connection to BAM file
     :param query_positions: query nucleotide positions in the pileup
-
+    :param cache_site_coverages_dict: cached coverages information for all or some positions (speed)
     :return: dictionary of positions and read counts
     '''
     coverages_list = [] #dict.fromkeys(query_positions)
@@ -20,12 +20,16 @@ def get_ref_coverage_array(bamfilepath, query_positions):
         pysam.index(bamfilepath, index_file)
     bamfile = pysam.AlignmentFile(bamfilepath, "rb")
 
-
     for position in query_positions:
-        coverage_tuple = bamfile.count_coverage(bamfile.get_reference_name(0),
-                                               quality_threshold=1, start=position-1,stop=position,
-                                               read_callback="nofilter")
+        if bamfilepath in cache_dict.keys() and cache_dict[bamfilepath].get(str(position)):
+            coverages_list.append(cache_dict[bamfilepath].get(str(position)))
+            #print("Using cache for position {}".format(position))
+        else:
+            #print("Getting coverage for position {}".format(position))
+            coverage_tuple = bamfile.count_coverage(bamfile.get_reference_name(0),
+                                                   quality_threshold=1, start=position-1,stop=position,
+                                                   read_callback="nofilter")
+            coverages_list.append(np.sum(coverage_tuple))
 
-        coverages_list.append(np.sum(coverage_tuple))
     return coverages_list
 
